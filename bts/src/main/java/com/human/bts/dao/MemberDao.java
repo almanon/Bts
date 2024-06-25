c82ea6e451fe7714bcc3fd07c093903f0af980dd
almanon <158257500+almanon@users.noreply.github.com>
2024-06-25 14:06:44 +0900

    Add files via upload
    dao add...

---
+++ bts/src/main/java/com/human/bts/dao/MemberDao.java
@@ -0,0 +1,20 @@
+package com.human.bts.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.human.bts.vo.*;
+
+public class MemberDao {
+	@Autowired
+	SqlSessionTemplate session;
+	
+	public int getLogin(MemberVO mVO) {
+		return session.selectOne("mSQL.login", mVO);
+	}
+	
+	
+	public int addMemb(MemberVO mVO) {
+		return session.insert("mSQL.addMemb", mVO);
+	}
+}
